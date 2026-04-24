79244c08c0a3600529c9822a54e1623fca98e23a
10439206 <gustonks15@gmail.com>
2026-04-24 12:12:06 -0300

    Exercício 7.2

--- conta.py
+++ conta.py
@@ -1,6 +1,14 @@
+
 class Conta:
     def __init__(self, saldo_inicial):
         self.saldo = saldo_inicial
 
     def investir_poupanca(self, valor):
         self.saldo += valor
+
+    def solicitar_cartao(self, usuario, api_credito, servico_notificacao):
+        # A api_credito e o servico_notificacao são dependências externas!
+        if api_credito.verificar_limite():
+            servico_notificacao.enviar_sms()
+            return "Cartão Aprovado"
+        return "Cartão Negado"
--- testes_conta.py
+++ testes_conta.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 from conta import Conta
 
 
@@ -7,10 +8,22 @@ class TestContaBancaria(unittest.TestCase):
     def test_investimento_poupanca_deve_aumentar_saldo(self):
         minha_conta = Conta(saldo_inicial=100.0)
         valor_investimento = 50.0
-
         minha_conta.investir_poupanca(valor_investimento)
-
         self.assertEqual(minha_conta.saldo, 150.0)
+
+    def test_solicitar_cartao_com_doubles(self):
+        minha_conta = Conta(saldo_inicial=100.0)
+
+        dummy_usuario = Mock()
+
+        stub_api = Mock()
+        stub_api.verificar_limite.return_value = True
+        mock_sms = Mock()
+        resultado = minha_conta.solicitar_cartao(dummy_usuario, stub_api, mock_sms)
+
+        self.assertEqual(resultado, "Cartão Aprovado")
+
+        mock_sms.enviar_sms.assert_called_once()
 
 
 if __name__ == '__main__':
