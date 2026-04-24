26655e093dc9f5f7131645488512ce4bc2f92068
10439206 <gustonks15@gmail.com>
2026-04-24 13:02:18 -0300

    Exercício 7.5

--- conta.py
+++ conta.py
@@ -1,4 +1,3 @@
-
 class Conta:
     def __init__(self, saldo_inicial):
         self.saldo = saldo_inicial
@@ -7,8 +6,12 @@ class Conta:
         self.saldo += valor
 
     def solicitar_cartao(self, usuario, api_credito, servico_notificacao):
-        # A api_credito e o servico_notificacao são dependências externas!
         if api_credito.verificar_limite():
             servico_notificacao.enviar_sms()
             return "Cartão Aprovado"
         return "Cartão Negado"
+
+
+    def calcular_rendimento(self):
+        rendimento = self.saldo * 0.005
+        self.saldo += rendimento
--- testes_conta.py
+++ testes_conta.py
@@ -8,7 +8,9 @@ class TestContaBancaria(unittest.TestCase):
     def test_investimento_poupanca_deve_aumentar_saldo(self):
         minha_conta = Conta(saldo_inicial=100.0)
         valor_investimento = 50.0
+
         minha_conta.investir_poupanca(valor_investimento)
+
         self.assertEqual(minha_conta.saldo, 150.0)
 
     def test_solicitar_cartao_com_doubles(self):
@@ -18,21 +20,22 @@ class TestContaBancaria(unittest.TestCase):
 
         stub_api = Mock()
         stub_api.verificar_limite.return_value = True
+
         mock_sms = Mock()
+
         resultado = minha_conta.solicitar_cartao(dummy_usuario, stub_api, mock_sms)
 
         self.assertEqual(resultado, "Cartão Aprovado")
 
         mock_sms.enviar_sms.assert_called_once()
 
+    def test_rendimento_poupanca(self):
+        minha_conta = Conta(saldo_inicial=1000.0)
+
+        minha_conta.calcular_rendimento()
+
+        self.assertEqual(minha_conta.saldo, 1005.0)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-# EXERCÍCIO 7.5 - TDD
-def test_rendimento_poupanca(self):
-    minha_conta = Conta(saldo_inicial=1000.0)
-
-       minha_conta.calcular_rendimento()
-
-        self.assertEqual(minha_conta.saldo, 1005.0)
